f055f1179227460a634bd2bd834a2ab9eb84cf6e
orikahmadov <orkhan.ahmadov28@.gmail.com>
2023-03-20 02:22:18 +0300

    added LSTM Model

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 symbols = ['AAPL', 'GOOGL', 'TSLA', 'MSFT', 'AMZN']
 timeframe = '1d'
@@ -94,7 +96,46 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
-print(f"Model accuracy: {accuracy:.2f}")
+
+# Prepare the data for LSTM
+def create_dataset(X, y, time_steps=1):
+    Xs, ys = [], []
+    for i in range(len(X) - time_steps):
+        v = X.iloc[i:(i + time_steps)].values
+        Xs.append(v)
+        ys.append(y.iloc[i + time_steps])
+    return np.array(Xs), np.array(ys)
+
+# LSTM model parameters
+time_steps = 10
+batch_size = 32
+epochs = 10
+
+# Prepare the data
+X = prepared_data['X']
+y = prepared_data['y']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Reshape the data into sequences for LSTM
+X_train_lstm, y_train_lstm = create_dataset(pd.DataFrame(X_train), pd.Series(y_train), time_steps)
+X_test_lstm, y_test_lstm = create_dataset(pd.DataFrame(X_test), pd.Series(y_test), time_steps)
+
+# Define the LSTM model
+model_lstm = Sequential()
+model_lstm.add(LSTM(50, activation='relu', input_shape=(X_train_lstm.shape[1], X_train_lstm.shape[2])))
+model_lstm.add(Dropout(0.2))
+model_lstm.add(Dense(1, activation='sigmoid'))
+model_lstm.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the LSTM model
+model_lstm.fit(X_train_lstm, y_train_lstm, epochs=epochs, batch_size=batch_size, verbose=1)
+
+# Evaluate the LSTM model
+lstm_accuracy = model_lstm.evaluate(X_test_lstm, y_test_lstm)[1]
+print(f"LSTM Model accuracy: {lstm_accuracy:.2f}")
+
+
+print(f"Random Forest Model accuracy: {accuracy:.2f}")
 
 signals = generate_signals(model, stock_data)
 print(signals)
